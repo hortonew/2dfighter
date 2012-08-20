ef9d4618656e5310af224d413237e6b6e65bf6c0
Erik Horton <hortonew@gmail.com>
2012-08-20 18:07:40 -0400

    Simplified gametext

--- classes/gametext.py
+++ classes/gametext.py
@@ -1,22 +1,15 @@
 import pygame
 
 class GameText:
-	def __init__(self):
-		t1, t1_rect = self.get_text('Controls', (0,0,0), (255,255,255), 600, 50, 36)
-		t2, t2_rect = self.get_text('SPACE - Jump', (0,0,0), (255,255,255), 600, 75, 20)
-		t3, t3_rect = self.get_text('SHIFT - Sprint', (0,0,0), (255,255,255), 600, 95, 20)
-		t4, t4_rect = self.get_text('WASD - Move', (0,0,0), (255,255,255), 600, 115, 20)
-		t5, t5_rect = self.get_text('R - Block', (0,0,0), (255,255,255), 600, 130, 20)
-		t6, t6_rect = self.get_text('CTRL - Shoot', (0,0,0), (255,255,255), 600, 145, 20)
+	def __init__(self):		
+		self.textobjects = []
 		
-		self.textobjects = [
-			[t1, t1_rect],
-			[t2, t2_rect],
-			[t3, t3_rect],
-			[t4, t4_rect],
-			[t5, t5_rect],
-			[t6, t6_rect]
-		]
+		self.textobjects.append(self.get_text('Controls', (0,0,0), (255,255,255), 600, 50, 36))
+		self.textobjects.append(self.get_text('SPACE - Jump', (0,0,0), (255,255,255), 600, 75, 20))
+		self.textobjects.append(self.get_text('SHIFT - Sprint', (0,0,0), (255,255,255), 600, 95, 20))
+		self.textobjects.append(self.get_text('WASD - Move', (0,0,0), (255,255,255), 600, 115, 20))
+		self.textobjects.append(self.get_text('R - Block', (0,0,0), (255,255,255), 600, 130, 20))
+		self.textobjects.append(self.get_text('CTRL - Shoot', (0,0,0), (255,255,255), 600, 145, 20))
 		
 	def get_text(self, text, color, bgcolor, cx, cy, size):
 		font = pygame.font.Font(None, size)
